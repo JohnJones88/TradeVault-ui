cbe52b0f1152fba8a64a12b1e97bef313577f81a
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2024-05-02 19:43:12 -0400

    moved api project to its own folder

---
+++ TradeVault-api/db/collectables-models.js
@@ -0,0 +1,29 @@
+const { sequelize } = require('./data-connections');
+const { DataTypes } = require('sequelize')
+
+const Collectables = sequelize.define('collectables', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING(64),
+    allowNull: false
+
+  },
+  description: {
+    type: DataTypes.STRING(64),
+    allowNull: false
+  },
+  age: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  condition: {
+    type: DataTypes.ENUM('mint', 'excellent', 'very good', 'poor')
+  }
+})
+
+
+module.exports = Collectables;
---
+++ TradeVault-api/routes/collectables-routes.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const router = express.Router();
+const { Sequelize } = require('sequelize');
+const Collectables = require('../db/collectables-models');
+const { validateToken } = require('../utils/authentication');
+
+router.get('/',/*validateToken,*/ async (req, res) => {
+  try {
+    //console.log(req.query.random)
+    let collectables = null
+    if (!req.query.random) {
+      collectables = await Collectables.findAll();
+
+    }
+    else {
+      collectables = await Collectables.findAll({ order: Sequelize.literal('rand()'), limit: parseInt(req.query.random) })
+
+    }
+    res.send(collectables)
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(`Internal Server Error ${error}`)
+  }
+})
+
+router.get('/:id', validateToken, async (req, res) => {
+  try {
+    const findCollectables = await Collectables.findByPk(req.params.id)
+    if (!findCollectables) {
+      res.status(404).send('Collectable is not found.');
+      return;
+    }
+    res.send(findCollectables);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(`Internal Server Error ${error}`)
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    if (!req.body.name) {
+      res.status(400).send('name must have a value.')
+      return;
+    }
+    if (!req.body.description) {
+      res.status(400).send('description must have a value.')
+      return;
+    }
+    if (!req.body.age) {
+      res.status(400).send('age must have a value.')
+      return;
+    }
+    if (!req.body.condition) {
+      res.status(400).send('condition must have a value.')
+      return;
+    }
+    let newCollectable = await Collectables.create({
+      name: req.body.name,
+      description: req.body.description,
+      age: req.body.age,
+      condition: req.body.condition
+
+    });
+    res.status(201).send(newCollectable);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(`Internal Server Error ${error}`)
+  }
+})
+
+module.exports = router;
