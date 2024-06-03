ce3639432f8f5f11b0cb658c38f19ca71af604b7
John Jones <jajones0622@gmail.com>
2024-06-02 21:14:51 -0400

    more changes to data connection

--- TradeVault-api/db/data-connections.js
+++ TradeVault-api/db/data-connections.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize');
 
 
 const sequelize = new Sequelize('collectables_db', process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST, 
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT, 
   dialect: 'mysql'
 });
 
